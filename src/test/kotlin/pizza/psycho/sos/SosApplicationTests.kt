77d698576246f3f5b77e89327384cea630f4406e
Richard Cho <abelsteiger@gmail.com>
2026-02-28 21:31:26 +0900

    fix: add missing test setup properties

--- src/test/kotlin/pizza/psycho/sos/SosApplicationTests.kt
+++ src/test/kotlin/pizza/psycho/sos/SosApplicationTests.kt
@@ -2,8 +2,10 @@ package pizza.psycho.sos
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SosApplicationTests {
     @Test
     fun contextLoads() {
