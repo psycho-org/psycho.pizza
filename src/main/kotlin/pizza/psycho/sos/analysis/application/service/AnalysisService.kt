72c8f1194759b3cd46204d6a1a0e3327a98862a2
dawwson <shines1427@gmail.com>
2026-03-04 08:03:37 +0900

    add: application file

---
+++ src/main/kotlin/pizza/psycho/sos/analysis/application/service/AnalysisService.kt
@@ -0,0 +1,9 @@
+package pizza.psycho.sos.analysis.application.service
+
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+@Transactional
+class AnalysisService {
+}
