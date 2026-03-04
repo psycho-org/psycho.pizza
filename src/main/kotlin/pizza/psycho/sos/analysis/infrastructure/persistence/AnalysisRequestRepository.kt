65d9c5a2b18bfe51a84f6c4093d340c7e61cbffd
dawwson <shines1427@gmail.com>
2026-03-04 15:38:35 +0900

    add: repository 추가

--- src/main/kotlin/pizza/psycho/sos/analysis/infrastructure/persistence/AnalysisRequestRepository.kt
+++ src/main/kotlin/pizza/psycho/sos/analysis/infrastructure/persistence/AnalysisRequestRepository.kt
@@ -2,8 +2,8 @@ package pizza.psycho.sos.analysis.infrastructure.persistence
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import pizza.psycho.sos.analysis.domain.entity.AnalysisReport
+import pizza.psycho.sos.analysis.domain.entity.AnalysisRequest
 import java.util.UUID
 
 @Repository
-interface AnalysisReportRepository : JpaRepository<AnalysisReport, UUID> {}
+interface AnalysisRequestRepository : JpaRepository<AnalysisRequest, UUID> {}
