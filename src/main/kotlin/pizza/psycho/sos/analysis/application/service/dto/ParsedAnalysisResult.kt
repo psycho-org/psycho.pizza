f5de3e1b9b8e4d748acef1136e42be8254254474
dawwson <shines1427@gmail.com>
2026-03-09 12:18:18 +0900

    comment: FIXME 주석 추가

--- src/main/kotlin/pizza/psycho/sos/analysis/application/service/dto/ParsedAnalysisResult.kt
+++ src/main/kotlin/pizza/psycho/sos/analysis/application/service/dto/ParsedAnalysisResult.kt
@@ -1,5 +1,6 @@
 package pizza.psycho.sos.analysis.application.service.dto
 
+// FIXME: 다음 PR에서 좀 더 구체화될 예정입니다.
 data class ParsedAnalysisResult(
     val summary: String,
     val risks: List<String>,
