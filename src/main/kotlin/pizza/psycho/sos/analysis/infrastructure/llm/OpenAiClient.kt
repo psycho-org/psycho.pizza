46296f1bedf894fe22579a9566f466ed4ae7e9c4
dawwson <shines1427@gmail.com>
2026-03-09 12:12:43 +0900

    comment: todo 주석 추가

--- src/main/kotlin/pizza/psycho/sos/analysis/infrastructure/llm/OpenAiClient.kt
+++ src/main/kotlin/pizza/psycho/sos/analysis/infrastructure/llm/OpenAiClient.kt
@@ -1,3 +1,8 @@
 package pizza.psycho.sos.analysis.infrastructure.llm
 
+// TODO:
+// 1. LLM API 비동기 호출 + callback url
+// 2. TODO: 접수 응답 + run_id
+// 3. TODO: job_id ←> run_id 매핑 저장
+// 4. callback url로 분석 결과 전송
 class OpenAiClient
