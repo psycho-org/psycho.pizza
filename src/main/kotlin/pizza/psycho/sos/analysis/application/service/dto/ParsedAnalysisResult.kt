ef99e47c9224062d47fbcfc5255dc6f56b13219e
dawwson <shines1427@gmail.com>
2026-03-14 16:40:24 +0900

    remove: 불필요한 파일 삭제

--- src/main/kotlin/pizza/psycho/sos/analysis/application/service/dto/ParsedAnalysisResult.kt
+++
@@ -1,9 +0,0 @@
-package pizza.psycho.sos.analysis.application.service.dto
-
-// FIXME: 다음 PR에서 좀 더 구체화될 예정입니다.
-data class ParsedAnalysisResult(
-    val summary: String,
-    val risks: List<String>,
-    val topIssues: List<String>,
-    val recommendations: List<String>,
-)
