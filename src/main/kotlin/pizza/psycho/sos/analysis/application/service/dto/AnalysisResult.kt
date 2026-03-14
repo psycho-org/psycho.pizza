bf4719fb625ba3d64c522002eacf525a6890cb15
dawwson <shines1427@gmail.com>
2026-03-14 16:40:53 +0900

    add: input dto

--- src/main/kotlin/pizza/psycho/sos/analysis/application/service/dto/AnalysisResult.kt
+++ src/main/kotlin/pizza/psycho/sos/analysis/application/service/dto/AnalysisResult.kt
@@ -3,6 +3,9 @@ package pizza.psycho.sos.analysis.application.service.dto
 import java.time.Instant
 import java.util.UUID
 
+/*
+ * 서비스 반환 DTO
+ */
 sealed interface AnalysisResult {
     data class Created(
         val id: UUID,
