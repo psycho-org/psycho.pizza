b797b78b57aab7f5bed0514954fc6ccf0ed1f681
dawwson <shines1427@gmail.com>
2026-03-17 21:41:26 +0900

    fix: after_install.sh 환경변수 추가

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -48,7 +48,7 @@ MAIL_TOKEN_VERIFY_SUCCESS_URL=$(get_param mail-token-verify-success-url)
 MAIL_TOKEN_VERIFY_FAILURE_URL=$(get_param mail-token-verify-failure-url)
 OPENAI_API_KEY=$(get_param api-key)
 REQUEST_QUEUE_NAME=$(get_param request-queue-name)
-RESPONSE_QUEUE_NAME=$(get_param response-queue-name)
+BUSINESS_NOTIFY_URL=$(get_param business-notify-sqs-queue-url)
 EOF
 
 # 보안: .env 파일 권한 제한
