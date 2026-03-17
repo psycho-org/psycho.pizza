1e4dea1d30171f6e48e8ca0473928bd46452dd56
dawwson <shines1427@gmail.com>
2026-03-17 20:12:48 +0900

    rename: config 패키지 위치 변경

--- src/main/kotlin/pizza/psycho/sos/analysis/infrastructure/sqs/config/SqsConfig.kt
+++ src/main/kotlin/pizza/psycho/sos/analysis/infrastructure/sqs/config/SqsConfig.kt
@@ -1,11 +1,13 @@
-package pizza.psycho.sos.common.config
+package pizza.psycho.sos.analysis.infrastructure.sqs.config
 
 import io.awspring.cloud.sqs.operations.SqsTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 
 @Configuration
+@Profile("!test")
 class SqsConfig {
     @Bean
     fun sqsTemplate(sqsAsyncClient: SqsAsyncClient): SqsTemplate = SqsTemplate.newTemplate(sqsAsyncClient)
